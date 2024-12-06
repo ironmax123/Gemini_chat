b3eae48778a10b0ad8b3c7249228255dad12522c
eita <>
2024-12-06 16:07:27 +0900

    言語一覧をコンポーネント化

---
+++ lib/pages/components/List_Views/lang_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+Widget langlistWidget(languageList, context) {
+  return Expanded(
+    child: SizedBox(
+      height: MediaQuery.of(context).size.height * 0.8,
+      child: ListView.builder(
+        itemCount: languageList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Text(
+            languageList[index],
+            style: const TextStyle(fontSize: 20.0),
+          );
+        },
+      ),
+    ),
+  );
+}
---
+++ lib/pages/lang_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import '../data/list.dart';
+import 'components/List_Views/lang_view.dart';
+
+class LangPage extends StatelessWidget {
+  final LanguageList languageList = LanguageList();
+
+  LangPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text('変換される言語一覧'),
+        ),
+        body: langlistWidget(languageList.lang_list, context));
+  }
+}
