94b4c255b84ffa7cd45b8f8355b9be0046e6a62f
eita <>
2024-12-06 15:07:38 +0900

    ディレクトリ変更

--- lib/pages/lang_page.dart
+++ lib/pages/lang_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'list.dart';
+import '../data/list.dart';
 
 class LangPage extends StatelessWidget {
   final LanguageList languageList = LanguageList();
