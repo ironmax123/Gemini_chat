fdf491e175baea2d6ea0a58602a9e9d6e98cfc40
eita <>
2024-12-09 13:33:37 +0900

    appBarに色を指定，空白の場合送信しない

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:gemini_chat/router/router.dart';
 
@@ -15,6 +14,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       routerConfig: goRouter,
+      title: 'Gemini_Chat',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
+        useMaterial3: true,
+      ),
     );
   }
 }
--- lib/pages/components/bottom_sheet/home_sheet.dart
+++ lib/pages/components/bottom_sheet/home_sheet.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 import 'package:gemini_chat/apis/send_data.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../lang/data/list.dart';
 
@@ -35,8 +38,13 @@ class BottomSheetWidget extends StatelessWidget {
             ),
             ElevatedButton(
               onPressed: () async {
-                await sending.sendingData(
-                    api, languageList, controller, onMessageSend);
+                controller.text.isNotEmpty
+                    ? await sending.sendingData(
+                        api, languageList, controller, onMessageSend)
+                    : ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('プロンプトが空白です')),
+                      );
+                context.pop();
               },
               child: const Text('送信'),
             ),
